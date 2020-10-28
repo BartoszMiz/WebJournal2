e08973bdde6ac14722fa503e0a2ee33897c2102a
BartoszMiz <bartmiz.2004@gmail.com>
2020-10-28 12:39:03 +0100

    Add a test controller
    Route: api/echo/{message}
    message - message to be sent back

---
+++ WebJournal2.API/Controllers/EchoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebJournal2.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EchoController : ControllerBase
+	{
+		[HttpGet("{message}")]
+		public IActionResult Get(string message)
+		{
+			return new OkObjectResult(message);
+		}
+	}
+}
--- WebJournal2.API/Program.cs
+++ WebJournal2.API/Program.cs
@@ -9,18 +9,19 @@ using Microsoft.Extensions.Logging;
 
 namespace WebJournal2.API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+					webBuilder.UseUrls("https://::1111");
+				});
+	}
 }
