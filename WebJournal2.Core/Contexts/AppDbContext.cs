d7aca97baa0dd96f0577b944ef58d9feb3fded26
BartoszMiz <bartmiz.2004@gmail.com>
2020-10-31 16:41:50 +0100

    Add JournalEntry

--- WebJournal2.Core/Contexts/AppDbContext.cs
+++ WebJournal2.Core/Contexts/AppDbContext.cs
@@ -7,5 +7,6 @@ namespace WebJournal2.Core.Contexts
 	{
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 		public DbSet<JournalPassword> Passwords { get; set; }
+		public DbSet<JournalEntry> Entries { get; set; }
 	}
 }
---
+++ WebJournal2.Core/Models/JournalEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebJournal2.Core.Models
+{
+	public class JournalEntry : DbEntity
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public DateTime SubmitDate { get; set; }
+	}
+}
