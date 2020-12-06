900f36893314560822eb29875b21617db2e64a2e
BartoszMiz <bartmiz.2004@gmail.com>
2020-12-06 16:22:12 +0100

    Add entry preview and model

---
+++ WebJournal2.Web.Core/JournalEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WebJournal2.Web.Core
+{
+	public class JournalEntry
+	{
+		public uint Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public DateTime Date { get; set; }
+
+		public JournalEntry() { }
+
+		public JournalEntry(uint id, string title, string content, DateTime date)
+		{
+			Id = id;
+			Title = title;
+			Content = content;
+			Date = date;
+		}
+
+		public string[] GetParagraphs()
+		{
+			return Content.Split("<==>");
+		}
+	}
+}
